740e163cf9876cb7e0f5e273272ac06b5c5991bd
jamesjoel <jamesjoelj@gmail.com>
2024-01-10 12:58:40 +0530

    some changes in header

--- controllers/SubCategoryController.js
+++ controllers/SubCategoryController.js
@@ -1,27 +1,27 @@
 const routes = require("express").Router();
-const Cate = require("../models/Category");
+const SubCategory = require("../models/SubCategory");
 
 routes.get("/", async(req, res)=>{
-    let result = await Cate.find();
+    let result = await SubCategory.find();
     res.send({ success : true, result : result });
 })
 routes.get("/:id", async(req, res)=>{
-    let result = await Cate.find({ _id : req.params.id });
+    let result = await SubCategory.find({ _id : req.params.id });
     res.send({ success : true, result : result[0] });
 })
 
 // { name : "Home Appliance" }
 routes.post("/", async(req, res)=>{
     // console.log(req.body)
-    await Cate.create(req.body);
+    await SubCategory.create(req.body);
     res.send({ success : true });
 })
 routes.put("/:id", async(req, res)=>{
-    await Cate.updateMany({ _id : req.params.id }, req.body);
+    await SubCategory.updateMany({ _id : req.params.id }, req.body);
     res.send({ success : true });
 })
 routes.delete("/:id", async(req, res)=>{
-    await Cate.deleteMany({ _id : req.params.id });
+    await SubCategory.deleteMany({ _id : req.params.id });
     res.send({ success : true });
 })
 
