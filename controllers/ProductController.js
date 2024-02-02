ea505cb32a1d1141b1ad76183291b3001def6ba7
jamesjoel <james.joelj@gmail.com>
2024-02-02 12:30:32 +0530

    file name change when file upload

--- controllers/ProductController.js
+++ controllers/ProductController.js
@@ -1,14 +1,13 @@
 const routes = require("express").Router();
 const Product = require("../models/Product");
 const path = require("path");
+const uni = require("unique-string-generator");
+
 
 routes.get("/", async(req, res)=>{
     let result = await Product.find();
     res.send({ success : true, result : result });
 })
-
-
-
 
 routes.get("/:id", async(req, res)=>{
     let result = await Product.find({ _id : req.params.id });
@@ -22,9 +21,15 @@ routes.post("/", async(req, res)=>{
     let body = JSON.parse(req.body.formdata);
     let image = req.files.image;
 
-    body.image = image.name;
+    
+    let arr = image.name.split(".");
+    let ext = arr[arr.length-1];
 
-    await image.mv(path.resolve()+"/assets/product-images/"+image.name);
+    let newname = uni.UniqueString()+"."+ext;
+    
+    body.image = newname;
+
+    await image.mv(path.resolve()+"/assets/product-images/"+newname);
 
     await Product.create(body);
     res.send({ success : true });
