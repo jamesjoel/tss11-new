dae50048573daf942aef711eca6d7e2f13fff856
jamesjoel <james.joelj@gmail.com>
2024-02-09 13:23:35 +0530

    sothing changes

--- controllers/SendMailController.js
+++ controllers/SendMailController.js
@@ -1,33 +1,20 @@
 const routes = require("express").Router();
 const nodemailer = require("nodemailer");
+const sendmail = require('sendmail')();
 
 routes.get("/", async(req, res)=>{
 
 
-    var transporter = nodemailer.createTransport({
-        service: 'gmail',
-        host: 'smtp.gmail.com',
-        auth: {
-          user: 'bhooshanpandre200@gmail.com',
-          pass: 'bhooshan2000@'
-        }
-      });
-      
-      var mailOptions = {
-        from: 'bhooshanpandre200@gmail.com',
-        to: 'james.steppingstone@gmail.com',
-        subject: 'Sending Email using Node.js',
-        text: 'That was easy!'
-      };
-      
-      transporter.sendMail(mailOptions, function(error, info){
-        if (error) {
-          console.log(error);
-        } else {
-          console.log('Email sent: ' + info.response);
-          res.send({ success : true });
-        }
-      });
+    sendmail({
+        from: 'test@test.org',
+        to: 'bhooshanpandre200@gmail.com',
+        subject: 'Hello World',
+        html: 'Hooray NodeJS!!!'
+      }, function (err, reply) {
+        console.log(err && err.stack)
+        console.dir(reply)
+        res.send({success : true})
+      })
 
 
 })
