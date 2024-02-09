c1a5bd15526e9c99e2f381a22d7ffe22f80d486c
jamesjoel <james.joelj@gmail.com>
2024-02-09 12:38:15 +0530

    live database connection code

--- config/db.js
+++ config/db.js
@@ -1,6 +1,16 @@
-require("mongoose").connect("mongodb://0.0.0.0:27017/tss11")
+// require("mongoose").connect("mongodb://0.0.0.0:27017/tss11")
+// .then(()=>{
+//     console.log("DB Connected")
+// }).catch((err)=>{
+//     console.log("DB NOT ----- Connected", err);
+// })
+require("mongoose").connect("mongodb+srv://jamessteppingstone:eZfqMZtXWY98IJpb@cluster0.h4svdn4.mongodb.net/")
 .then(()=>{
     console.log("DB Connected")
 }).catch((err)=>{
     console.log("DB NOT ----- Connected", err);
 })
+
+
+// username : jamessteppingstone
+// password : eZfqMZtXWY98IJpb
